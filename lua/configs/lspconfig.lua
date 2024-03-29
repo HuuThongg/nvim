57cc8b53a4ed5f8c583151fcd22b08bda97c5b30
HuuThongg <lehuuthong0506@gmail.com>
2024-03-29 00:36:30 -0700

    b

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,10 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls","" }
+local servers = { "html", "cssls", "gopls", "tailwindcss" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -21,3 +21,21 @@ lspconfig.tsserver.setup {
   on_init = on_init,
   capabilities = capabilities,
 }
+
+-- go
+-- lspconfig.gopls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   cmd = { "gopls" },
+--   filetypes = { "go", "gomod", "gowork", "gotmpl" },
+--   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+--   settings = {
+--     gopls = {
+--       completeUnimported = true,
+--       usePlaceholders = true,
+--       analyses = {
+--         unusedparams = true,
+--       },
+--     },
+--   },
+-- }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -14,28 +14,88 @@ return {
       require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
+    -- opts = {
+    --   servers = {
+    --     tailwindcss = {}
+    --   }
+    -- }
   },
   --
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier",
-        "gopls","gofumpt","goimports","gomodifytags","goimports-revisier",
-        "sqls","tailwindcss",
-  		},
-  	},
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "prettierd",
+        "gopls",
+        "gofumpt",
+        "goimports",
+        "gomodifytags",
+        "goimports-reviser",
+        "golines",
+        "sqls",
+        "tailwindcss",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+      },
+    },
   },
   --
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css","javascript","typescript","tsx",
-        "go","rust","slang",
-  		},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "go",
+        "rust",
+        "slang",
+      },
+    },
+  },
+  -- {
+  --   "williamboman/mason-lspconfig.nvim",
+  -- },
+  {
+    "windwp/nvim-ts-autotag",
+    lazy = false,
+    opts = {
+      autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+        filetypes = { "html", "xml", "tsx" },
+      },
+    },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup {
+        css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css.
+        javascript = { rgb_fn = true }, -- Enable parsing rgb(...) functions in javascript.
+        html = { names = true }, -- Enable parsing color names in html.
+      }
+    end,
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      }
+    }
+  },
+  {
+    "github/copilot.vim",
+    lazy = false,
   },
 }
